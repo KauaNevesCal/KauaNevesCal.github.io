8f79ba3a0e109348b14e04bf6832249d5c5f043a
KauaNevesCal <kaua.caldeira@estudante.ifms.edu.br>
2025-01-30 11:26:02 -0400

    oi

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-sm bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/#">DW4</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                    <a class="nav-link" href="#">Teste 1</a>
+                    <a class="nav-link" href="#">Teste 2</a>      
+            </ul>
+        </div>
+    </div>
+</nav>
